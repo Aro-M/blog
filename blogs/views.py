5ef6acb9ffca672f2b69242f422f1a2438813e3f
Aro-M <ar.militosyan.97@mail.ru>
2023-01-12 14:09:22 +0400

    Hi

---
+++ blogs/views.py
@@ -0,0 +1,11 @@
+from  django.views.generic import  ListView
+from .models import   Post
+from django.views.generic import  ListView,DetailView
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class  BlogDetailView(DetailView):
+    model =  Post
+    template_name = 'post_detail.html'
