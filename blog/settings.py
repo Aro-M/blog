8cdb68f1251d1519ce99efce682e0fc2c4ea4ce7
Aro-M <ar.militosyan.97@mail.ru>
2023-01-12 17:16:19 +0400

    added

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs.apps.BlogsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView # new
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+
 urlpatterns = [
-path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), # new
+path("post/create/", BlogCreateView.as_view(), name="post_new"),
+path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+path("post/<int:pk>/edit/",BlogUpdateView.as_view(),name='post_edit'),
+path("post/<int:pk>/delete/",BlogDeleteView.as_view(),name='post_delete'),
 path("", BlogListView.as_view(), name="home"),
 ]
